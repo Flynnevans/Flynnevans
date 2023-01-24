58a7b24421901d7d248f0b3261ed3b8a9e403eec
flynn <flynnevans2005@icloud.com>
2023-01-24 22:00:49 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	venv/Mainpage.py

--- venv/testing/test2.py
+++ venv/testing/test2.py
@@ -1,2 +1,12 @@
-x = round(5.82345*4)
-print(x/4)
+import sqlite3
+
+def UpdateTable():
+    # here wil will show how to update a record in the table Deprtements.
+
+    DeptNo = int(input("Which department you want to update/ Enter Dept ID   "))
+    NewPhoneNumber = input("Enter new number ")
+
+    conn = sqlite3.connect('CompanyStaff.db')
+    conn.execute('''UPDATE DEPARTMENTS  SET Telephone = ? WHERE DEp_ID = ? ''', (NewPhoneNumber, DeptNo))
+    conn.commit()
+    conn.close()
